1586c98022b9b63309b9383cbe8b07a2f578b54a
Prince Thind <princethind799@gmail.com>
2022-08-25 13:04:57 +0530

    implement basic downloading

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function (req, res, next) {
+
+  const files = [];
+  fs.readdir('./download', (err, data) => {
+    for (const fileOrDir of data) {
+      if (fileOrDir == '.gitkeep') continue;
+      if (fileOrDir.split(".").length == 1) continue;
+      files.push(fileOrDir)
+    }
+    res.render('index', { files });
+
+  })
+});
+
+router.get('/download/:name', function (req, res, next) {
+  const name = req.params.name;
+  res.download('./download/'+name)
 });
 
 module.exports = router;
